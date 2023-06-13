b62d63e6703595ea79583bcaf5efbfe0fdf12721
erickvneri <bardovasco@protonmail.com>
2023-06-13 09:46:03 -0600

    lua server config

--- nvim_lua/lua/settings/setup_lsp.lua
+++ nvim_lua/lua/settings/setup_lsp.lua
@@ -1,4 +1,55 @@
+-- Copyright 2023 erickvneri
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 local lsp = require("lsp-zero")
+local lsp_config = require("lspconfig")
+
+local function _lua_server_setup()
+  lsp_config.lua_ls.setup({
+    settings = {
+      Lua = {
+        runtime = {
+          version = "Lua 5.3",
+          path = {
+            "?.lua",
+            "?/init.lua",
+            "/usr/local/share/lua/5.3/?.lua/",
+            "/usr/local/share/lua/5.3/?/init.lua",
+            "/usr/local/lib/lua/5.3/?.lua",
+            "/usr/local/lib/lua/5.3/?/init.lua",
+            "/usr/share/lua/5.3/?.lua",
+            "/usr/share/lua/5.3/?/init.lua",
+            "/home/asdeoips/.st_lua_libs/lua_libs-api_v5/?.lua",
+            "/home/asdeoips/.st_lua_libs/lua_libs-api_v5/?/init.lua",
+            "./?.lua",
+            "./?/init.lua"
+          }
+        }
+      }
+    }
+  })
+end
+
+local function _error_diagnostics_setup()
+  vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+    underline = true,
+    severity_sort = false,
+    float = true,
+  })
+end
 
 local function setup()
   lsp.preset("recommended")
@@ -61,15 +112,12 @@ local function setup()
   lsp.setup()
 
   --
+  -- Lua server setup
+  _lua_server_setup()
+
+  --
   -- Error diagnostics
-  vim.diagnostic.config({
-    virtual_text = true,
-    signs = true,
-    update_in_insert = false,
-    underline = true,
-    severity_sort = false,
-    float = true,
-  })
+  _error_diagnostics_setup()
 end
 
 return setup
